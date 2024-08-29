810f45dd38e70e1f7d11285ed78c2c150d72de16
tolgabayrakdev <bayraktolga28@gmail.com>
2024-08-29 23:31:35 +0300

    erro hadmling in service

--- server/app/controller/authentication_controller.py
+++ server/app/controller/authentication_controller.py
@@ -3,17 +3,17 @@ from app.service.authentication_service import AuthenticationService
 from app.schema.authentication_schema import LoginUser
 
 router = APIRouter()
+auth_service = AuthenticationService()
 
-@router.post("/login", status_code=201)
+@router.post("/login")
 async def login(user: LoginUser, response: Response):
-    result = AuthenticationService.login(user.email, user.password)
-    if result:
+        result = auth_service.login(email=user.email, password=user.password)
+
         response.set_cookie(
-            key="access_token", value=result["access_token"], httponly=True  # type: ignore
+            key="access_token", value=result["access_token"], httponly=True
         )
         response.set_cookie(
-            key="refresh_token", value=result["refresh_token"], httponly=True  # type: ignore
+            key="refresh_token", value=result["refresh_token"], httponly=True
         )
         return {"message": "Login is successful."}
-    else:
-        raise HTTPException(status_code=500, detail="Server error!")
+
--- server/app/service/authentication_service.py
+++ server/app/service/authentication_service.py
@@ -3,16 +3,22 @@ from fastapi import HTTPException
 from ..model import User
 from app.util.helper import Helper
 
-db = SessionLocal()
-helper = Helper()
 
 class AuthenticationService:
 
-    @staticmethod
-    def login(email: str, password: str) -> HTTPException | dict[str, str]:
-        user = db.query(User).filter_by(email=email).first()
-        if user is None or not helper.match_hash_text(str(user.password), password):
-            raise HTTPException(status_code=400, detail="Email or password wrong!")
-        access_token = helper.generate_access_token({"user_id": user.id})
-        refresh_token = helper.generate_refresh_token({"user_id": user.id})
-        return {"access_token": access_token, "refresh_token": refresh_token}
+    def __init__(self):
+        self.helper = Helper()
+
+    def login(self, email: str, password: str) -> dict[str, str]:
+        db = SessionLocal()
+        try:
+            user = db.query(User).filter_by(email=email).first()
+            if user is None or not self.helper.match_hash_text(str(user.password), password):
+                raise HTTPException(status_code=400, detail="Email or password wrong!")
+            access_token = self.helper.generate_access_token({"user_id": user.id})
+            refresh_token = self.helper.generate_refresh_token({"user_id": user.id})
+            return {"access_token": access_token, "refresh_token": refresh_token}
+        except Exception:
+            raise HTTPException(status_code=500, detail="An unexpected server error occurred.")
+        finally:
+            db.close()
